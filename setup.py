7e9402534f2c3c08651432ba01111520964e8c1c
Andrés Trífero <atrifero@ladiaria.com.uy>
2020-06-09 10:57:29 -0300

    modified setup.py

--- setup.py
+++ setup.py
@@ -3,24 +3,14 @@
 from os.path import exists
 from setuptools import setup, find_packages
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
 setup(
     name='django-adminlte3',
-    # version=open('VERSION').read().strip(),
+    version='0.1.6',
     author='d3n1z',
     author_email='d3n1z@protonmail.com',
-    packages=find_packages(),
-    scripts=[],
-    url='https://github.com/d-demirci/django-adminlte3',
+    packages=['adminlte3', 'adminlte3_theme'],
+    url='https://github.com/virusereturns/django-adminlte3',
     license='MIT',
     description='Admin LTE templates, admin theme, and template tags for Django',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     include_package_data=True,
-    # Any requirements here, e.g. "Django >= 1.1.1"
-    install_requires=[
-        'django',
-    ],
-    # python_requires='>=3.6',
 )
